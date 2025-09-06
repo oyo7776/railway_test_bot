785422cb661c6f85f64ff372896d04f038a7341d
oyo7776 <152698829+oyo7776@users.noreply.github.com>
2025-09-06 12:08:30 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,4 +8,5 @@ def hello():
     return "Hello from Railway! 🚀"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Railway gives PORT
+    app.run(host="0.0.0.0", port=port)
